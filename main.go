4fb2e04ccd9fb78ad7fe1f36a0ab1c3f9b166f6a
wowBlaBla <wow1992122@gmail.com>
2023-03-08 04:53:35 +0900

    add fizzbuzz controller

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	controller "github.com/wowBlaBla/FizzBuzz-API/controllers"
 )
 
 func main() {
 	router := gin.Default()
+	router.POST("/fizzbuzz", controller.FizzBuzzController)
 
 	router.Run(":3001")
 }
